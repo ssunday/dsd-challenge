a1e96857f14a56e47d6d1bc3b4b8ff4f9b6e17d9
Josh Schwartzman <josh@dreamseedo.org>
2020-07-28 09:11:51 -0700

    Add logo into header

--- app/javascript/src/components/Header.js
+++ app/javascript/src/components/Header.js
@@ -9,20 +9,42 @@ const NavLink = styled(Link)`
   display: inline-block;
   text-decoration: none;
   color: ${colors.white};
-  margin-left: 15px;
+  margin-left: ${(props) => (props.noLeftMargin ? 0 : '15px')};
+`
+
+const Brand = styled(Typography)`
+  color: ${colors.white};
 `
 
 const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Grid container justify="space-between" alignItems="center">
+        <Grid
+          container
+          justify="space-between"
+          alignItems="center"
+          justifyContent="center"
+        >
           <Grid item>
-            <Typography variant="h6">Dream See Do</Typography>
+            <Grid container alignItems="center">
+              <Grid item>
+                <NavLink to="/" noLeftMargin>
+                  <img
+                    src="https://s3.amazonaws.com/assets.dreamseedo.org/challenge/cloud-logo.png"
+                    width="60"
+                  />
+                </NavLink>
+              </Grid>
+              <Grid item>
+                <Brand variant="h6" color={colors.white}>
+                  <NavLink to="/">Dream See Do</NavLink>
+                </Brand>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item>
             <Typography variant="body1">
-              <NavLink to="/">Home</NavLink>
               <NavLink to="/courses">Courses</NavLink>
               <NavLink to="/users/me">My Account</NavLink>
             </Typography>
